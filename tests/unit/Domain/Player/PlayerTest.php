db84cede0850b84d36b5ee15911a15b2966c74e6
Mina Nabil Sami <mina.nsami@gmail.com>
2018-10-30 15:00:09 +0100

    More player tests.

--- tests/unit/Domain/Player/PlayerTest.php
+++ tests/unit/Domain/Player/PlayerTest.php
@@ -3,6 +3,8 @@
 declare(strict_types = 1);
 
 use PHPUnit\Framework\TestCase;
+use TicTacToe\Domain\Player\Exception\SorryPlayerNameIsTooLong;
+use TicTacToe\Domain\Player\Exception\SorryPlayerNameIsTooShort;
 use TicTacToe\Domain\Player\Player;
 use TicTacToe\Domain\Player\PlayerId;
 use TicTacToe\Domain\Player\PlayerName;
@@ -43,5 +45,17 @@ class PlayerTest extends TestCase
         self::assertEquals('y', $player->playingToken());
         self::assertInstanceOf(PlayerId::class, $player->id());
         self::assertNotNull($player->id());
+    }
+
+    public function testItThrowsExceptionIfPlayerNameIsShort()
+    {
+        self::expectException(SorryPlayerNameIsTooShort::class);
+        Player::createPlayerWithTokenX("m");
+    }
+
+    public function testItThrowsExceptionIfPlayerNameIsLong()
+    {
+        self::expectException(SorryPlayerNameIsTooLong::class);
+        Player::createPlayerWithTokenX("FGLAHETLDGHAELTRHG53Y693576935sfkhglkshfgshfgkfkgherg53yt3589gh358gh359hg395gh395");
     }
 }
