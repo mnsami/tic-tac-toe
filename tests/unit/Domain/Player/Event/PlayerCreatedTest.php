9506df55283d7a9ad6fbf1e2d4da406635cc2a7c
Mina Nabil Sami <mina.nsami@gmail.com>
2019-06-11 23:11:45 +0200

    green tests for PlayerCreated.

--- tests/unit/Domain/Player/Event/PlayerCreatedTest.php
+++ tests/unit/Domain/Player/Event/PlayerCreatedTest.php
@@ -9,18 +9,25 @@ class PlayerCreatedTest extends \PHPUnit\Framework\TestCase
 {
     public function testItExposesCorrectData()
     {
-        $player = $this->createPlayer();
+        $player = $this->createPlayerX();
         $event = new PlayerCreated($player);
 
         self::assertEquals($player->createdAt()->getTimestamp(), $event->occurredAt()->getTimestamp());
 
         $payload = $event->toArray();
+        self::assertEquals(PlayerCreated::class, $event->name());
+
         self::assertArrayHasKey('playerId', $payload);
+        self::assertEquals((string) $player->id(), $payload['playerId']);
+
         self::assertArrayHasKey('playerName', $payload);
+        self::assertEquals($player->name(), $payload['playerName']);
+
         self::assertArrayHasKey('playerToken', $payload);
+        self::assertEquals($player->playingToken(), $payload['playerToken']);
     }
 
-    public function createPlayer($name = 'foobar')
+    public function createPlayerX($name = 'foobar')
     {
         $player = Player::createPlayerWithTokenX($name);
 
