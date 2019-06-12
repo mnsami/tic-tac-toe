3d4c6ff038c7136313e9bc2fdef63aaf38318e04
Mina Nabil Sami <mina.nsami@gmail.com>
2019-06-13 00:00:58 +0200

    green GameId tests

--- tests/unit/Engine/Domain/Model/Game/GameIdTest.php
+++ tests/unit/Engine/Domain/Model/Game/GameIdTest.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\Uuid;
-use TicTacToe\Domain\Game\GameId;
+use TicTacToe\Engine\Domain\Model\Game\GameId;
 
 class GameIdTest extends TestCase
 {
